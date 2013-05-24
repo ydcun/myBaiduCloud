699f749cc8d649a437460045cb0c8450ce8161c3
ydcun <ydcun@163.com>
2013-05-24 10:55:37 +0800

    普通修改，可以下载文件，有测试和应用的token

--- src/com/util/AccessToken.java
+++ src/com/util/AccessToken.java
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
+
 package com.util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -28,20 +30,21 @@ public class AccessToken {
 	
 
 	/**
-	 * @param conn通过get方式获取json串
+	 * @param conn通过get方式获取StringBuffer
 	 * @return
 	 */
-	private String getJsonString(URLConnection conn) {
+	private StringBuffer getJsonString(URLConnection conn) {
 		InputStreamReader isr = null;
 		BufferedReader br = null;
 		StringBuffer sb = null;
 		try {
-			isr = new InputStreamReader(conn.getInputStream(), "utf-8");
+			isr = new InputStreamReader(conn.getInputStream(),"gb2312");
 			br = new BufferedReader(isr);
 			String line = null;
 			sb = new StringBuffer();
 			while ((line = br.readLine()) != null) {
 				sb.append(line);
+				sb.append("\r\n");
 			}
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
@@ -57,7 +60,7 @@ public class AccessToken {
 			}
 		}
 
-		return sb.toString();
+		return sb;
 	}
 
 	/**
@@ -69,7 +72,7 @@ public class AccessToken {
 		URL u = new URL(Info.URL_INFO + "?method=" + Info.METHOD_INFO + "&access_token="+ Info.ACCESS_TOKEN);
 		URLConnection conn = u.openConnection();// 打开网页链接
 		// 获取用户云盘信息
-		String cloudJson = this.getJsonString(conn);
+		String cloudJson = this.getJsonString(conn).toString();
 
 		// 解析成对象
 		Gson gson = new Gson();
@@ -78,13 +81,30 @@ public class AccessToken {
 		return cloudInfo;
 	}
 
-	public String downFile(String path) throws Exception{
-		path ="20130403.log";
+	/**
+	 * @param path  下载的文件在云盘中的路径 包括文件名
+	 * @param savePath 下载文件保存到本地的路径 包括文件名
+	 * @return
+	 * @throws Exception
+	 */
+	public String downFile(String path,String name,String savePath) throws Exception{
+		name="20130403.log";
+		path ="/apps/mybaidu/20130403.log";
+		savePath= name;
+		
+		
 		URL u = new URL(Info.URL_UPLOAD+"?path="+path+"&method="+Info.METHOD_DOWN+"&access_token="+Info.ACCESS_TOKEN);
 		URLConnection conn = u.openConnection();// 打开网页链接
-		String cloudJson = this.getJsonString(conn);
+		StringBuffer fileBuffer = this.getJsonString(conn);
+		//将文件从内存中输出
+		File saveFile = new File(savePath);
+		BufferedWriter buWriter = new BufferedWriter(new FileWriter(saveFile));
+		buWriter.write(fileBuffer.toString());
+		buWriter.flush();
 		return null;
 	}
+
+
 	/**
 	 * @param path 云盘存放路径
 	 * @param name 要上传的文件
@@ -95,6 +115,7 @@ public class AccessToken {
 		//模拟文件
 		file = new File("accessToken.jpg");
 		path="%2Fapps%2F%E6%B5%8B%E8%AF%95%E5%BA%94%E7%94%A8%2F";//    "/apps/测试应用/"
+		
 		
 		//将需要url传值的参数和url组装起来
 		String u =Info.URL_UPLOAD+"?path="+path+file.getName()+"&method="+Info.METHOD_UPLOAD+"&access_token="+Info.ACCESS_TOKEN+"&ondup="+Info.ONDUP_CMM;
@@ -136,152 +157,9 @@ public class AccessToken {
 	}
 
 	public static void main(String[] args) throws Exception {
-		new AccessToken().uploadFile("/",null);
-//		new AccessToken().downFile(null);
+//		new AccessToken().uploadFile("/",null);
+		new AccessToken().downFile(null,null,null);
 //		new AccessToken().getCloudInfo();
 	}
 }
-=======
-package com.util;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
-import com.entity.CloudInfo;
-import com.google.gson.Gson;
-
-/**
- * @author ydcun 获取百度开发所需的accesToken
- *         说明网址：http://developer.baidu.com/wiki/index.php
- *         ?title=docs/pcs/rest/file_data_apis_list ；
- *         最新的： token：
- *         3.92666f2d7ca22dc9f19372e38532346e
- *         .2592000.1371880919.1091082467-879176 user name:ydcun
- * */
-public class AccessToken {
-	
-	
-
-	/**
-	 * @param conn通过get方式获取json串
-	 * @return
-	 */
-	private String getJsonString(URLConnection conn) {
-		InputStreamReader isr = null;
-		BufferedReader br = null;
-		StringBuffer sb = null;
-		try {
-			isr = new InputStreamReader(conn.getInputStream(), "utf-8");
-			br = new BufferedReader(isr);
-			String line = null;
-			sb = new StringBuffer();
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally{
-			try {
-				if(isr!=null)
-					isr.close();
-			} catch (IOException e) {
-				System.out.println("流关闭是异常");
-				e.printStackTrace();
-			}
-		}
-
-		return sb.toString();
-	}
-
-	/**
-	 * @return
-	 * @throws Exception
-	 *             获取云空间的信息
-	 */
-	public CloudInfo getCloudInfo() throws Exception {
-		URL u = new URL(Info.URL_INFO + "?method=" + Info.METHOD_INFO + "&access_token="+ Info.ACCESS_TOKEN);
-		URLConnection conn = u.openConnection();// 打开网页链接
-		// 获取用户云盘信息
-		String cloudJson = this.getJsonString(conn);
-
-		// 解析成对象
-		Gson gson = new Gson();
-		CloudInfo cloudInfo = gson.fromJson(cloudJson, CloudInfo.class);
-		System.out.println("云盘信息："+cloudInfo);
-		return cloudInfo;
-	}
-
-	public String downFile(String path) throws Exception{
-		path ="20130403.log";
-		URL u = new URL(Info.URL_UPLOAD+"?path="+path+"&method="+Info.METHOD_DOWN+"&access_token="+Info.ACCESS_TOKEN);
-		URLConnection conn = u.openConnection();// 打开网页链接
-		String cloudJson = this.getJsonString(conn);
-		return null;
-	}
-	/**
-	 * @param path 云盘存放路径
-	 * @param name 要上传的文件
-	 * @return
-	 * @throws Exception 
-	 */
-	public boolean uploadFile(String path,File file) throws Exception{
-		//模拟文件
-		file = new File("accessToken.jpg");
-		path="%2Fapps%2F%E6%B5%8B%E8%AF%95%E5%BA%94%E7%94%A8%2F";//    "/apps/测试应用/"
-		
-		//将需要url传值的参数和url组装起来
-		String u =Info.URL_UPLOAD+"?path="+path+file.getName()+"&method="+Info.METHOD_UPLOAD+"&access_token="+Info.ACCESS_TOKEN+"&ondup="+Info.ONDUP_CMM;
-		URL url = new URL(u);
-		
-		//将文件读取到内存中来
-		FileReader fin = new FileReader(file);
-		BufferedReader buReader = new BufferedReader(fin);
-		StringBuffer sb = new StringBuffer();
-		String line=null;
-		while((line = buReader.readLine())!=null){
-			sb.append(line);
-		}
-		
-	    HttpURLConnection httpurlconnection = null;
-	    try
-	    {
-	      httpurlconnection = (HttpURLConnection) url.openConnection();
-	      httpurlconnection.setDoOutput(true);
-	      httpurlconnection.setRequestMethod("POST");
-	      
-	      httpurlconnection.getOutputStream().write(sb.toString().getBytes());
-	      httpurlconnection.getOutputStream().flush();
-	      httpurlconnection.getOutputStream().close();
-	      int code = httpurlconnection.getResponseCode();
-	      System.out.println("code   " + code);
-
-	    }
-	    catch(Exception e)
-	    {
-	      e.printStackTrace();
-	    }
-	    finally
-	    {
-	      if(httpurlconnection!=null)
-	        httpurlconnection.disconnect();
-	    }
-		return true;
-	}
-
-	public static void main(String[] args) throws Exception {
-		new AccessToken().uploadFile("/",null);
-//		new AccessToken().downFile(null);
-//		new AccessToken().getCloudInfo();
-	}
-}
->>>>>>> b642f14f331aeca62aa669b3669d73810f16eae8
--- src/com/util/Info.java
+++ src/com/util/Info.java
@@ -1,16 +1,19 @@
-<<<<<<< HEAD
+
 package com.util;
 
 /**
  * @author ydcun
  * 参数参考http://developer.baidu.com/wiki/index.php?title=docs/pcs/rest/file_data_apis_list
+ *mybaidu token:3.ffe1020e20a52f873600aa8ef07fa609.2592000.1371955887.1091082467-879176
+ *测试应用 token:3.a2250ddd95ad14ac620148e632ab4b2f.2592000.1371890458.1091082467-248414
+ *
  *
  */
 public interface Info {
 	/**
 	 * 链接的token令牌
 	 */
-	String ACCESS_TOKEN = "3.a2250ddd95ad14ac620148e632ab4b2f.2592000.1371890458.1091082467-248414";// token
+	String ACCESS_TOKEN = "3.ffe1020e20a52f873600aa8ef07fa609.2592000.1371955887.1091082467-879176";// token
 	/**
 	 * 获取云盘信息时的method参数值
 	 */
@@ -42,48 +45,4 @@ public interface Info {
 	 */
 	String ONDUP_CMM="newcopy";
 }
-=======
-package com.util;
 
-/**
- * @author ydcun
- * 参数参考http://developer.baidu.com/wiki/index.php?title=docs/pcs/rest/file_data_apis_list
- *
- */
-public interface Info {
-	/**
-	 * 链接的token令牌
-	 */
-	String ACCESS_TOKEN = "3.a2250ddd95ad14ac620148e632ab4b2f.2592000.1371890458.1091082467-248414";// token
-	/**
-	 * 获取云盘信息时的method参数值
-	 */
-	String METHOD_INFO="info";
-	/**
-	 * 上传文件时METHOD的参数值
-	 */
-	String METHOD_UPLOAD="upload";
-	/**
-	 * 下载文件时的METHOD参数值
-	 */
-	String METHOD_DOWN="download";
-	
-	/**
-	 * 获取云盘空间的URL
-	 */
-	String URL_INFO = "https://pcs.baidu.com/rest/2.0/pcs/quota";
-	/**
-	 * 上传/下载文件时的URL
-	 */
-	String URL_UPLOAD = "https://pcs.baidu.com/rest/2.0/pcs/file";
-	
-	/**
-	 * 遇到重复文件覆盖ondup参数的值
-	 */
-	String ONDUP_FG="overwrite";
-	/**
-	 * 遇到重复文件重命名    命名规则为    （文件名_日期.后缀）
-	 */
-	String ONDUP_CMM="newcopy";
-}
->>>>>>> b642f14f331aeca62aa669b3669d73810f16eae8
